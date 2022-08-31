9961a3886d548420aff875defbd04539c81aa0e7
Theodor M <e1426389@student.tuwien.ac.at>
2022-08-31 02:06:42 +0200

    Improve output and logging in general.

--- prepare.sh
+++ prepare.sh
@@ -6,9 +6,12 @@ FONTSTYLEDIR="${OUTD}/fontstyles"
 FONTFILEDIR="${OUTD}/fonts"
 FONTHOSTDIR="../fonts"
 LOG="log.txt"
+
+echo "==================== $(date --iso-8601=seconds) ===================="
+
 mkdir -p "${FONTSTYLEDIR}"
 
-for fontfamily in {Noto+Sans,Noto+Serif,Inconsolata,Bitter,Open+Sans,Roboto,Roboto+Slab,Lato,Source+Sans+Pro,Merriweather,Montserrat,Libre+Franklin,Raleway,Roboto+Slab}; do
+for fontfamily in {Noto+Sans,Noto+Serif,Inconsolata,Bitter,Open+Sans,Roboto,Roboto+Slab,Lato,Source+Sans+Pro,Merriweather,Montserrat,Libre+Franklin,Raleway,Roboto+Slab,Ubuntu}; do
 
 fontdir="${FONTFILEDIR}/"
 fontname=$(echo "$fontfamily" |  tr '[:upper:]' '[:lower:]' | sed 's/[^A-Za-z0-9 _\-]//g')
@@ -19,34 +22,34 @@ touch "${fontcssfile}"
 for fontweight in {black,bold,semibold,regular,light,extralight}; do
 for fontstyle in {regular,italic}; do
 for fontsubset in {latin,latin-ext}; do
+	fontcode="${fontname}_${fontweight}_${fontstyle}_${fontsubset}"
 
-	echo "==== ==== ==== ====" >> ${LOG}
+	echo "==== ==== ==== ==== ${fontcode} @ $(date --iso-8601=seconds)" | tee -a ${LOG}
 	gapiurl="https://fonts.googleapis.com/css?family=${fontfamily}:${fontweight}${fontstyle}&subset=${fontsubset}&display=swap"
-	fontcss=$(curl "${gapiurl}")
-	echo "gapiurl=${gapiurl}" >> log.txt
+	fontcss=$(curl -s "${gapiurl}")
+	echo "gapiurl=${gapiurl}" | tee -a ${LOG}
 	if $( echo "${fontcss}" | grep -q -e "400: Missing font family" -e "not available"); then
-		echo "error: unavailable=${gapiurl}" >> ${LOG}
+		echo "error: unavailable=${gapiurl}" | tee -a ${LOG}
 		continue;
 	fi
-	echo "fontcss=${fontcss}" >> ${LOG}
+	#echo "fontcss=${fontcss}" | tee -a ${LOG}
 
 	fonturl=$(echo "${fontcss}" | grep "src:.*url" | sed 's/^.*url(\(.*\)) format(\(.*\)).*$/\1/g')
 	fonturlfile=$(basename "${fonturl}")
 	fonturlpath=$(dirname "${fonturl}" | sed 's#^.*\.com/s/##g')
-	fontcode="${fontname}_${fontweight}_${fontstyle}_${fontsubset}"
 	fontfile="${fontdir}/${fonturlpath}/${fontcode}.font"
-	echo "fonturl=${fonturl}" >> ${LOG}
-	echo "fonturlpath=${fonturlpath}" >> ${LOG}
+	echo "fonturl=${fonturl}" | tee -a ${LOG}
+	echo "fonturlpath=${fonturlpath}" | tee -a ${LOG}
 	mkdir -p "${fontdir}/${fonturlpath}"
 	if [ -f "$FILE" ]; then
-		echo "info: already=${fontfile}" >> ${LOG}
+		echo "info: skipped=${fontfile}" | tee -a ${LOG}
 		continue;
 	fi
-	curl "${fonturl}" >> ${fontfile}
+	curl -s "${fonturl}" >> ${fontfile}
 
 	csssrcs="  src: local('')"
 	for format in {ttf,eot,svg,woff,woff2}; do
-		echo "fontforge -lang=ff -c 'Open(\$1); Generate(\$2); Close();' ${fontfile} ${fontfile}.${format}"
+		echo "fontforge -quiet -lang=ff -c 'Open(\$1); Generate(\$2); Close();' ${fontfile} ${fontfile}.${format}"
 	done | bash
 	csssrcs+=$(echo -e ",\n     url(${FONTHOSTDIR}/${fonturlpath}/${fontcode}.font.woff2) format('woff2')")
 	csssrcs+=$(echo -e ",\n     url(${FONTHOSTDIR}/${fonturlpath}/${fontcode}.font.woff) format('woff')")
